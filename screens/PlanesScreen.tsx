e4fee17803725aa2cc18e336323c1191b5910f8c
Maxi De Filippis <mdefilippis@encamina.com>
2023-01-30 00:23:20 -0300

    Agrego cards de seguros a PlanesScreen

--- screens/PlanesScreen.tsx
+++ screens/PlanesScreen.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import PlanesCard from "../components/PlanesCard";
 
 const PlanesScreen = () => {
   return (
-    <View>
-      <Text>PlanesScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <PlanesCard
+          headerText={"Contra todo riesgo"}
+          pricePerMonth={"$9.488"}
+          totalPrice={"$1.596.000"}
+        />
+        <PlanesCard
+          headerText={"Seguro de terceros"}
+          pricePerMonth={"$15.000"}
+          totalPrice={"$1.200.500"}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 export default PlanesScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    backgroundColor: "gray",
+  },
+});
