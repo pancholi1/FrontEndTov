ef8e9a6b95c37d5b06c15ea9f1a985197ecc56de
Francisco Porta <franciscoporta19@gmail.com>
2023-05-16 13:59:12 -0300

    push

--- screens/LoginScreen/Login.tsx
+++ screens/LoginScreen/Login.tsx
@@ -13,8 +13,8 @@ import {
 import { RootStackScreenProps } from "../../types";
 import { LinearGradient } from "expo-linear-gradient";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { database, firebaseConfig } from "../../firebase-config";
+import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { database, firebaseConfig, providerGoogle } from "../../firebase-config";
 import { initializeApp } from "firebase/app";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { setUser } from "../../navigation/redux/slices/user";
@@ -26,6 +26,7 @@ const Login = ({ navigation }: RootStackScreenProps<"Login">) => {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const dispatch = useAppDispatch();
+
 
   const handleSingIn = async () => {
     try {
@@ -49,6 +50,34 @@ const Login = ({ navigation }: RootStackScreenProps<"Login">) => {
       console.log(error);
     }
   };
+
+  const handleSingInWithGoogle = async () => {
+    try{
+      signInWithPopup(auth, providerGoogle)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        console.log('token', token);
+        // The signed-in user info.
+        const user = result.user;
+        console.log('user', user);
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    });
+    } catch (error){
+      console.log('error', error)
+    }
+  }
 
   return (
     <ScrollView style={{ width: "100%", backgroundColor: "#130C34" }}>
@@ -96,14 +125,24 @@ const Login = ({ navigation }: RootStackScreenProps<"Login">) => {
                 scrollEnabled={true}
               />
             </LinearGradient>
+              <Pressable style={styles.containter_contraseña}>
+                <Text style={styles.olvidaste_contraseña}>¿Olvidaste tu contraseña?</Text>
+                </Pressable>
             <View style={styles.container_buton}>
-              <Pressable onPress={() => handleSingIn()}>
+              <Pressable onPress={() => handleSingIn()} style={styles.button_login}>
                 <LinearGradient
                   colors={["#0995a6", "#112044"]}
                   style={styles.login_button}
                 >
                   <Text style={styles.login_butontext}> Ingresar </Text>
                 </LinearGradient>
+              </Pressable>
+            </View>
+            
+            <View style={styles.container_button}>
+              <Pressable onPress={() => handleSingInWithGoogle()} style={styles.button_google} >
+                <Image source={require('../../assets/images/Login/img_google.png')} style={styles.img_google}/>
+                <Text style={styles.text_google}>Ingresar con Google</Text>
               </Pressable>
             </View>
 
@@ -141,10 +180,6 @@ const styles = StyleSheet.create({
   login_containerInput: {
     width: "80%",
   },
-  container_buton: {
-    marginTop: 10,
-    width: "80%",
-  },
   input: {
     width: "100%",
     borderColor: "#B39AE7",
@@ -165,19 +200,29 @@ const styles = StyleSheet.create({
     paddingBottom: 25,
     fontFamily: "Poppins_Regular",
   },
+  
+  container_buton: {
+    width: "100%",
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
   login_button: {
     width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
     borderRadius: 15,
     padding: "3%",
+  },
+
+  button_login:{
+    width:'70%'
   },
 
   login_butontext: {
     fontSize: 18,
     color: "#DED3F4",
     fontFamily: "Poppins_ExtraBold",
+    textAlign:'center'
   },
 
   login_containerhelp: {
@@ -216,6 +261,35 @@ const styles = StyleSheet.create({
     color: "white",
     opacity: 2,
   },
+  container_button:{
+    width:'55%',
+    borderRadius:20,
+    padding:'2%',
+    margin:'5%',
+    borderWidth: 2,
+    borderColor: '#B39AE7',
+  },
+  button_google: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text_google:{
+    color:'#B39AE7',
+    fontFamily: "Poppins_Regular",
+    fontSize: 13,
+    fontWeight:'600',
+    marginLeft:'10%'
+  },
+  img_google:{
+    marginLeft:5
+  },
+  containter_contraseña:{
+    margin:'2%',
+    marginRight:'40%'
+  },
+  olvidaste_contraseña:{
+    color:'#B39AE7'
+  }
 });
 
 export default Login;
